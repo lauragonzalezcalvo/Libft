66d20978f330d888cf196f1591b883cfe93b37b0
lauragonzalezcalvo <laura24gc@gmail.com>
2023-03-16 10:27:02 +0100

    Add prototype on lifbt.h

--- libft.h
+++ libft.h
@@ -24,7 +24,7 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-//size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 void *ft_memset(void *b, int c, size_t len);
 
 size_t  ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
