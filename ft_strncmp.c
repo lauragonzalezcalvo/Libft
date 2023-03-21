56fd40a3f4552056ac4973753fc48641dacf13e5
lauragonzalezcalvo <laura24gc@gmail.com>
2023-03-21 19:32:11 +0100

    Add changes on ft_stncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,18 +21,27 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	str1 = (char *)s1;
 	str2 = (char *)s2;
 	i = 0;
-	if (n == 0)
+	// if (n == 0)
+	// {
+	// 	return (0);
+	// }
+	// while (str1[i] == str2[i] && (i < n - 1)
+	// 	&& str1[i])
+	// 	i++;
+	// return (str1[i] - str2[i]);
+	while(str1[i] !='\0' && i < n -1)
 	{
-		return (0);
-	}
-	while (str1[i] == str2[i] && (i < n - 1)
-		&& str1[i])
+		if(str1[i] > str2[i])
+			return(1);
+		else if(str1[i] < str2[i])
+			return (-1);
 		i++;
-	return (str1[i] - str2[i]);
+	}
+	return(0);
 }
 
 int	main(void)
 {
-	printf("%d\n", strncmp("hello", "hola", 2));
-    printf("%d\n", ft_strncmp("hello", "hola", 2));
+	printf("%d\n", strncmp("hello", "ha", 2));
+    printf("%d\n", ft_strncmp("hello", "ha", 2));
 }
