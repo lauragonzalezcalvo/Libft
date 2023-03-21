2ab249026298b00ca483eb4241cd523f1fd8156b
laurgonzalezcalvo <laura24gc@gmail.com>
2023-03-21 20:51:55 +0100

    Add end strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: laurgonz <laurgonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:59:17 by laurgonz          #+#    #+#             */
-/*   Updated: 2023/03/21 17:14:36 by laurgonz         ###   ########.fr       */
+/*   Updated: 2023/03/21 20:50:52 by laurgonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,21 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	char	*str1;
-	char	*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	str1 = (char *)s1;
-	str2 = (char *)s2;
 	i = 0;
-	// if (n == 0)
-	// {
-	// 	return (0);
-	// }
-	// while (str1[i] == str2[i] && (i < n - 1)
-	// 	&& str1[i])
-	// 	i++;
-	// return (str1[i] - str2[i]);
-	while(str1[i] !='\0' && i < n -1)
-	{
-		if(str1[i] > str2[i])
-			return(1);
-		else if(str1[i] < str2[i])
-			return (-1);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (((str1[i] != '\0' && str2[i] != '\0')
+			&& (str1[i] == str2[i])) && i < n)
 		i++;
-	}
-	return(0);
+	return (str1[i] - str2[i]);
 }
-
+/*
 int	main(void)
 {
-	printf("%d\n", strncmp("hello", "ha", 2));
-    printf("%d\n", ft_strncmp("hello", "ha", 2));
-}
+	printf("%d\n", strncmp("hello", "hellos", 7));
+    printf("%d\n", ft_strncmp("hello", "hellos", 7));
+}*/
