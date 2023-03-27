22bbd27cb24dad3729f76adc4786a44ccbb0ef5f
laurgonzalezcalvo <laura24gc@gmail.com>
2023-03-27 13:59:48 +0200

    Add strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,50 +6,40 @@
 /*   By: laurgonz <laurgonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 10:42:27 by laurgonz          #+#    #+#             */
-/*   Updated: 2023/03/27 11:55:55 by laurgonz         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:54:16 by laurgonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-size_t	ft_strlen(const char *s)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	cont;
-
-	cont = 0;
-	while (s[cont] != '\0')
-		cont++;
-	return (cont);
-}
-
-char *ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str1;
-	char	*str2;
 	char	*dest;
 	size_t	i;
 	size_t	l;
 
 	i = 0;
 	l = 0;
-	str1 = (char *)s1;
-	str2 = (char *)s2;
-	dest = malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
-	while ( i < ft_strlen(str1))
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (dest == 0)
+		return (NULL);
+	while (i < ft_strlen(s1))
 	{
-		dest[i] = str1[i];
+		dest[i] = s1[i];
 		i++;
 	}
-	while ( l < ft_strlen(str2))
+	while (l < ft_strlen(s2))
 	{
-		dest[i + l] = str2[l];
+		dest[i + l] = s2[l];
 		l++;
 	}
 	dest[i + l] = '\0';
-    return dest;		
+	return (dest);
 }
-
+/*
 int	main(void)
 {
 	printf("%s", ft_strjoin("hola","que tal"));
-}
+}*/
