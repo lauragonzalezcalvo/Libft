fed01a6f531872f4297829271db5d8e3a4eea6f0
laurgonzalezcalvo <laura24gc@gmail.com>
2023-04-01 17:32:21 +0200

    Add atoi complete

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: laurgonz <laurgonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:34:02 by laurgonz          #+#    #+#             */
-/*   Updated: 2023/04/01 15:25:49 by laurgonz         ###   ########.fr       */
+/*   Updated: 2023/04/01 17:25:52 by laurgonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"libft.h"
@@ -14,29 +14,34 @@
 int	ft_atoi(const char *str)
 {
 	char	*a;
-	int	i;
-	int	n;
-	int	number;
+	int		i;
+	int		n;
+	int		number;
 
 	a = (char *)str;
 	i = 0;
 	n = 1;
 	number = 0;
-	while ((a[i] >= 9 && a[i] <= 23) || a[i] == ' ' || str[i] == '+')
+	while ((a[i] >= 9 && a[i] <= 13) || a[i] == ' ' )
+	{
 		i++;
-	if (a[i] == '-')
+	}
+	if (a[i] == '-' || a[i] == '+')
+	{
+		if (a[i] == '-')
 			n = -1;
-	i++;
+		i++;
+	}
 	while (a[i] >= '0' && a[i] <= '9')
 	{
-		number = (number * 10) + (str[i] - '0');
+		number = (number * 10) + (a[i] - '0');
 		i++;
 	}
 	return (number * n);
 }
-
+/*
 int	main(void)
 {
-	printf("%d\n", atoi("++47"));
-	printf("%d\n", ft_atoi("++47"));	
-}
+	printf("%d\n", atoi("\01615"));
+	printf("%d\n", ft_atoi("\01615"));	
+}*/
