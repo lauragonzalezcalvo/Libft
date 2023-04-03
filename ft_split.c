da8e7fde4d5f68789d61338e4824a93942144e56
laurgonzalezcalvo <laura24gc@gmail.com>
2023-04-03 11:27:48 +0200

    Doing ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: laurgonz <laurgonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 18:19:03 by laurgonz          #+#    #+#             */
-/*   Updated: 2023/04/01 19:10:34 by laurgonz         ###   ########.fr       */
+/*   Updated: 2023/04/03 11:27:28 by laurgonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* DESCRIPCIÓN/
@@ -15,9 +15,40 @@
  * VALOR DEVUELTO
  * Devuelve un array de arrays, una matriz, con las strings resultantes de la 
  * separación.
- * */
-/*
+ */
+
+#include"libft.h"
+
+int	ft_countwords(char const *s, char c)
+{
+	int	i;
+	int	counter;
+
+	i = 0;
+	counter = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && s[i-1] == c)
+			counter++;
+		i++;
+	}
+	return(counter);
+}
+
+char **ft_split(char const *s, char c)
+{
+	char	**numberwords;
+	// porque ponemos un char si lo que devolvemos es un int
+	// necesitaos saber el numero de palabras para malloc
+	numberwords = (char **)malloc(sizeof(char *) * (ft_countwords(s, c)) + 1);
+	//printf("%d", ft_countwords(s, c));
+	
+
+	return(numberwords);
+}
+
 int main(void)
 {
-    printf("%d", ft_split("h,e l l o", ' '));
-}*/
+	printf("%d", ft_countwords("---hola--que--tal", '-'));
+	printf("%s", *ft_split("---hola--que--tal", '-'));
+}
